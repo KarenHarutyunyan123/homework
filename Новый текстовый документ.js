95fc191cb167d7a5b15639e15e64c46d4c3cb0b4
KarenHarutyunyan123 <99826332+KarenHarutyunyan123@users.noreply.github.com>
2022-03-15 18:12:37 +0400

    Add files via upload

---
+++ Новый текстовый документ.js
@@ -0,0 +1,28 @@
+/2
+a = sumAll(1, 12, 4);
+
+function sumAll() {
+  var i;
+  var sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+///////////////
+/4 
+const num=window.prompt();
+const str = num.toString();
+const result = [str[0]];
+  
+for(let x=1; x<str.length; x++)
+  {
+    if((str[x-1]%2 === 0)&&(str[x]%2 === 0))
+     {
+      result.push('-', str[x]);
+     }
+    else
+     {
+      result.push(str[x]);
+     }
+  }
+console.log(result.join(''));
